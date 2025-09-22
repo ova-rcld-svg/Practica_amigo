8b24e7d4f620c2cb73ff31f7cff52d1d4af15293
Osvaldo Recalde <orecalde@teco.com.ar>
2025-09-22 13:12:36 -0300

    Funcion para limpiar despues del sorteo

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-// Creamos un array vacío para guardar los nombres
 let amigos = [];
 
-// Función para agregar un nombre al array
 function agregarAmigo() {
-    // Obtenemos el valor escrito en el input
-    let input = document.getElementById("amigo");
-    let nombre = input.value; 
+    let nombre = document.getElementById("amigo").value;
 
-      if (nombre === "") {
-        alert("Por favor escribe un nombre válido");
-        return; // salimos si está vacío
+    if (nombre === "") {
+        alert("Escribe un nombre");
+    } else {
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+        mostrarLista();
     }
-
-    amigos.push(nombre);
-    input.value = ""; // limpiar campo
-
-    mostrarLista(); // llamamos a la función que actualiza la lista
 }
 
 function mostrarLista() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = ""; // limpiar antes de pintar de nuevo
-
-    // Recorremos con un bucle for y mostramos con numeración
+    let lista = "";
     for (let i = 0; i < amigos.length; i++) {
-        lista.innerHTML += "<li>" + (i + 1) + ". " + amigos[i] + "</li>";
+        lista += "<li>" + (i + 1) + ". " + amigos[i] + "</li>";
     }
+    document.getElementById("listaAmigos").innerHTML = lista;
 }
 
-// Función para sortear un amigo secreto
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert("No hay nombres para sortear");
     } else {
         let indice = Math.floor(Math.random() * amigos.length);
-        let resultado = document.getElementById("resultado");
-        resultado.innerHTML = "<li>🎉 El amigo secreto es: " + amigos[indice] + "</li>";
-    }
+        document.getElementById("resultado").innerHTML =
+            "<li>🎉 El amigo secreto es: " + amigos[indice] + "</li>";
+          // 🔹 Reiniciar todo después del sorteo
+        amigos = [];
+        document.getElementById("listaAmigos").innerHTML = "";
+        document.getElementById("amigo").value = "";         
+    
+        }
 }
